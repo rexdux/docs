5903a1c885560570462f113f66b07bfc14213551
Robson Niemeyer <51690424+rexdux@users.noreply.github.com>
2025-11-27 19:42:35 +0000

    Add index HTML page

--- generate-html.sh
+++ generate-html.sh
@@ -3,7 +3,14 @@
 for filepath in "$@"; do
   if [ -e "$filepath" ]; then
     filename=$(basename "$filepath" .md)
-    echo "Generating pages/${filename}.html"
-    pandoc "$filepath" -o "pages/${filename}.html" --template=layouts/default.html
+    if [ "$filename" != "index" ]; then
+      # Generate the HTML in pages directory
+      echo "Generating pages/${filename}.html"
+      pandoc "$filepath" -o "pages/${filename}.html" --template=layouts/default.html
+    else
+      # Generate index.html in root directory
+      echo "Generating ${filename}.html"
+      pandoc "$filepath" -o "${filename}.html" --template=layouts/index.html
+    fi
   fi
 done
