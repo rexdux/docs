4a6128b6efd548409529a54340b8192d17a5295e
Robson Niemeyer <51690424+rexdux@users.noreply.github.com>
2025-11-27 16:40:16 +0000

    Update script

--- generate-html.sh
+++ generate-html.sh
@@ -3,6 +3,7 @@
 for filepath in "$@"; do
   if [ -e "$filepath" ]; then
     filename=$(basename "$filepath" .md)
+    echo "Generating pages/${filename}.html"
     pandoc "$filepath" -o "pages/${filename}.html" --template=layouts/default.html
   fi
 done
